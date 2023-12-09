a8d122517905d1bbe7c3f903824001d5a2cbfe7e
Mo <mohamed@way2b1.com>
2023-12-08 16:17:28 -0800

    wip

--- src/pages/api/upload/index.ts
+++ src/pages/api/upload/index.ts
@@ -4,6 +4,8 @@ import formidable from "formidable";
 import fs from "fs";
 import { v4 as generateUniqueCode } from "uuid";
 
+// const PDFToTextLambda
+
 // Configure AWS S3
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -16,63 +18,101 @@ const s3 = new AWS.S3();
 export const config = { api: { bodyParser: false } };
 
 const uploadHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "POST") {
-    res.status(405).json({ error: "Method not allowed" });
-    return;
-  }
-
-  const form = formidable();
-
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      res.status(500).json({ error: "Error parsing the form data." });
+  try {
+    if (req.method !== "POST") {
+      res.status(405).json({ error: "Method not allowed" });
       return;
     }
 
-    const file = files?.file && files?.file[0];
+    const form = formidable();
 
-    if (!file) {
-      res.status(400).json({ error: "No file provided" });
-      return;
-    }
-
-    // Read the file and get its content
-    fs.readFile(file.filepath, (readError, fileContent) => {
-      if (readError) {
-        res.status(500).json({ error: "Error reading file" });
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        res.status(500).json({ error: "Error parsing the form data." });
         return;
       }
 
-      // Set parameters for S3 upload
-      const params: AWS.S3.PutObjectRequest = {
-        Bucket: process.env.AWS_S3_BUCKET_NAME as string,
-        Key: generateUniqueCode(), // Generate unique file name
-        Body: fileContent,
-      };
+      const file = files?.file && files?.file[0];
 
-      if (file.mimetype) {
-        params.ContentType = file.mimetype;
+      if (!file) {
+        res.status(400).json({ error: "No file provided" });
+        return;
       }
 
-      // Upload file to S3
-      s3.upload(params, (uploadError, data) => {
-        if (uploadError) {
-          console.error("uploadError", uploadError);
-
-          res.status(500).json({
-            error: "Error uploading file",
-            details: uploadError.message,
-          });
-
+      // Read the file and get its content
+      fs.readFile(file.filepath, (readError, fileContent) => {
+        if (readError) {
+          res.status(500).json({ error: "Error reading file" });
           return;
         }
 
-        res
-          .status(200)
-          .json({ message: "File uploaded successfully", data: data.Location });
+        const key = file.originalFilename + "_" + generateUniqueCode();
+
+        // Set parameters for S3 upload
+        const params: AWS.S3.PutObjectRequest = {
+          Bucket: process.env.AWS_S3_BUCKET_NAME as string,
+          Key: key,
+          Body: fileContent,
+        };
+
+        if (file.mimetype) {
+          params.ContentType = file.mimetype;
+        }
+
+        // Upload file to S3
+        s3.upload(params, async (uploadError, data) => {
+          try {
+            if (uploadError) {
+              throw uploadError;
+            }
+
+            // Extract text from PDF
+            const response = await extractTextFromPdf(key);
+            console.log("response", response);
+
+            res.status(200).json({
+              message: "File uploaded successfully",
+              s3_location: data.Location,
+            });
+          } catch (error) {
+            console.error("Error uploading file", error);
+            res.status(500).json({ error: "Error uploading file" });
+            return;
+          }
+        });
       });
     });
-  });
+  } catch (error) {
+    console.error("Error uploading file", error);
+    res.status(500).json({ error: "Error uploading file" });
+  }
+};
+
+const extractTextFromPdf = async (key: string) => {
+  try {
+    const lambda = new AWS.Lambda();
+
+    const params = {
+      FunctionName: "pdf-to-text",
+      InvocationType: "RequestResponse", // 'Event' for asynchronous
+      Payload: JSON.stringify({
+        body: {
+          bucket: process.env.AWS_S3_BUCKET_NAME as string,
+          key: key,
+        },
+      }),
+    };
+
+    // Invoke Lambda function
+    const result = await lambda.invoke(params).promise();
+    const payload = JSON.parse(result.Payload as string);
+    console.log("payload", payload);
+
+    return JSON.parse(payload.body);
+  } catch (error) {
+    console.error("Error extracting text from PDF", error);
+    throw error;
+  }
 };
 
 export default uploadHandler;
